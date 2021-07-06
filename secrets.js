082ae3c3be2f53bd578458c3ebb6b720b1cc9ee7
Vlad Sopin <comme.des.vlados@gmail.com>
2021-07-06 18:35:20 +0300

    rm

--- secrets.js
+++ secrets.js
@@ -7,8 +7,3 @@
 //     secret : 'c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3',
 //     mnemonic : 'useful proud scene expire muscle gentle book skull system repeat fox slow'
 // };
-
-module.exports = {
-    secret : '49d8a543dc48e29dbc4d59440a91163ef19a2006aca65ba0536a4bba85ab0c56',
-    mnemonic : 'solve brief sleep fury wild blame stereo hurdle veteran earth deal wise'
-};
